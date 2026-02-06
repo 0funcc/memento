506e4cae6869a3bfa3deb43a30e94641740c8aa9
Ali <westbrookcricket1@gmail.com>
2026-02-06 21:44:11 +0300

    added two main view files

---
+++ Memento/Views/AllView.swift
@@ -0,0 +1,13 @@
+// displays all tasks
+
+import SwiftUI
+
+struct AllView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    AllView()
+}
---
+++ Memento/Views/TodayView.swift
@@ -0,0 +1,18 @@
+// displays tasks that are due today
+
+import SwiftUI
+
+struct TodayView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("Today")
+        }
+    }
+}
+
+#Preview {
+    TodayView()
+}
