359e54606869b89364205ec1f074207eeab513ab
Ali <westbrookcricket1@gmail.com>
2026-02-06 18:43:49 +0300

    Initial Commit

---
+++ Memento/MementoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoApp.swift
+//  Memento
+//
+//  Created by Ali Sajulake Abdul Gafur on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MementoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
